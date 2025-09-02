c82886ac90b8417791646bb5e3425a704f5babf5
enissamil <113582640+enissamil@users.noreply.github.com>
2025-09-02 10:42:55 +0300

    Update CityRepository.java

--- backend/repository/CityRepository.java
+++ backend/repository/CityRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CityRepository extends JpaRepository<City, Integer> {
+public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> findByName(String name);
 }
